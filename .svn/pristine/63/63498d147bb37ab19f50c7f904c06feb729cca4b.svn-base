cedd33e4875cc8ba468c7a45d8d69b0d21f19452
Shikanime <deva.shikanime@outlook.com>
2018-03-26 16:51:10 +0200

    update

--- .svn/pristine/63/63498d147bb37ab19f50c7f904c06feb729cca4b.svn-base
+++
@@ -1,8 +0,0 @@
-package models
-
-data class User(
-  val firstName: String,
-  val lastName: String,
-  val email: String,
-  val password: String
-)
--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -2,4 +2,3 @@ const val databaseUrl = "jdbc:postgresql://localhost:5432/resto"
 const val databaseDriver = "org.postgresql.Driver"
 const val databaseUser = "resto"
 const val databasePassword = "evolution"
-
